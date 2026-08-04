1c29fdda4f6925ee38628588b187fe7332201b83
Demo-work <beheraanurag6@gmail.com>
2026-08-04 18:04:30 +0530

    hii

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ems-frontend-virid-mu.vercel.app/"],
     credentials: true,
   }),
 );
